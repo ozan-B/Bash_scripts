b3fb3e6695a7907f7bf464290e9903adfb7afb1f
ozan bozkurt <ozanbozkurt1974@gmail.com>
2024-01-04 01:54:52 -0600

    .

--- hash_comparater.sh
+++ hash_comparater.sh
@@ -11,6 +11,9 @@ NC='\033[0m' # No Color
 dosya1="$1"
 dosya2="$2"
 
+#eğer araç yanlış kullanıldıysa bir uyarı ver 
+#exit 1 ifadesi, bir bash betiğinde kullanılan bir çıkış kodunu belirtir. aracın çalışmasını sonlandırır
+#-z ifadesi, bir dizedeki uzunluğun sıfır (zero) olup olmadığını kontrol etmek için kullanılır.Dize boşsa (uzunluğu sıfırsa), -z ifadesi (true) olur.
 if [ -z "$dosya1" ] || [ -z "$dosya2" ]; then
 	echo -e "\n"
     echo -e "${RED}Kullanım: ${GREEN}$0 dosya1 dosya2"
@@ -18,6 +21,7 @@ if [ -z "$dosya1" ] || [ -z "$dosya2" ]; then
 fi
 
 # SHA-256 hash hesaplamak için sha256sum komutunu kullanır 
+#awk ilk komutun çıktısının sadece ilk satırını okumak için kullanılır
 hash1=$(sha256sum "$dosya1" | awk '{print $1}')
 hash2=$(sha256sum "$dosya2" | awk '{print $1}')
 
